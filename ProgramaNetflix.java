9e8229045b1880ec85f7f162dfffbb27aa8bc9db
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2023-10-08 13:56:13 -0300

    Initial commit

---
+++ ProgramaNetflix.java
@@ -0,0 +1,17 @@
+public class ProgramaNetflix {
+    private String id;
+    private String title;
+    private String type; //TODO: confirm type
+    private String description;
+    private int release_year;
+    private String age_certification; //TODO: confirm type
+    private int runtime;
+    private String[] genres; //TODO: confirm type
+    private String[] production_countries; //TODO: confirm type
+    private int seasons; //TODO: confirm type
+    private String imdb_id;
+    private Double imdb_score; //TODO: confirm type
+    private int imdb_votes; //TODO: confirm type
+    private Double tmdb_popularity; //TODO: confirm type
+    private Double tmdb_score; //TODO: confirm type
+}
