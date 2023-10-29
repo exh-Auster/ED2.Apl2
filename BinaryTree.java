3bf9a994a7b23736b0bff625c8688b5896ed9008
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2023-10-29 13:06:07 -0300

    Initial binary tree commit

---
+++ BinaryTree.java
@@ -0,0 +1,103 @@
+/**
+ * //TODO
+ * 
+ * @author André Matteucci - 32273541
+ * @author Felipe Ribeiro  - 32212720
+ * @author Enzo Koji       - 32273754
+*/
+
+public class BinaryTree {
+    private Node root;
+
+    public BinaryTree() {
+        this.root = null;
+    }
+
+    public BinaryTree(Node root) {
+        this.root = root;
+    }
+
+    public boolean isEmpty() {
+        return this.root == null;
+    }
+
+    public Node getRoot() {
+        return this.root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public String inorderTraversal() {
+		return inorderTraversal(root);
+	}
+
+	/**
+	 * Percorre a árvore em ordem simétrica.
+	 * 
+	 * @param node nó de início do percurso
+	 * @return     string com a expressão em ordem simétrica
+	 */
+	private String inorderTraversal(Node node) {
+		if (node == null) {
+			return "";
+		}
+
+		StringBuilder traversal = new StringBuilder();
+		
+		traversal.append(inorderTraversal(node.getLeft()));
+		traversal.append(node.getData() + " | "); //TODO
+		traversal.append(inorderTraversal(node.getRight()));
+		
+		return traversal.toString();
+	}
+
+	public String preorderTraversal() {
+		return preorderTraversal(root);
+	}
+
+	/**
+	 * Percorre a árvore em pré-ordem.
+	 * 
+	 * @param node nó de início do percurso
+	 * @return     string com a expressão em pré-ordem
+	 */
+	private String preorderTraversal(Node node) {
+		if (node == null) {
+			return "";
+		}
+
+		StringBuilder traversal = new StringBuilder();
+		
+		traversal.append(node.getData() + " | "); //TODO
+		traversal.append(preorderTraversal(node.getLeft()));
+		traversal.append(preorderTraversal(node.getRight()));
+		
+		return traversal.toString();
+	}
+
+	public String postorderTraversal() {
+		return postorderTraversal(root);
+	}
+
+	/**
+	 * Percorre a árvore em pós-ordem.
+	 * 
+	 * @param node nó de início do percurso
+	 * @return     string com a expressão em pós-ordem
+	 */
+	private String postorderTraversal(Node node) {
+		if (node == null) {
+			return "";
+		}
+
+		StringBuilder traversal = new StringBuilder();
+
+		traversal.append(postorderTraversal(node.getLeft()));
+		traversal.append(postorderTraversal(node.getRight()));
+		traversal.append(node.getData() + " | "); //TODO
+		
+		return traversal.toString();
+	}
+}
---
+++ Node.java
@@ -0,0 +1,159 @@
+/**
+ * //TODO
+ * 
+ * @author André Matteucci - 32273541
+ * @author Felipe Ribeiro  - 32212720
+ * @author Enzo Koji       - 32273754
+ * 
+ * @see BinaryTree
+*/
+
+public class Node {
+    protected Node parent;
+    protected Node left;
+    protected Node right;
+    protected ProgramaNetflix data;
+
+    public Node() {
+        this.parent = null;
+        this.left = null;
+        this.right = null;
+        this.data = null;
+    }
+
+    public Node(Node parent) {
+        this.parent = parent;
+        this.left = null;
+        this.right = null;
+        this.data = null;
+    }
+
+    public Node(Node left, Node right) {
+        this.parent = null;
+        this.left = left;
+        this.right = right;
+        this.data = null;
+    }
+
+    public Node(Node parent, Node left, Node right) {
+        this.parent = parent;
+        this.left = left;
+        this.right = right;
+        this.data = null;
+    }
+
+    public Node(ProgramaNetflix data) {
+        this.parent = null;
+        this.left = null;
+        this.right = null;
+        this.data = data;
+    }
+
+    public Node(Node parent, ProgramaNetflix data) {
+        this.parent = parent;
+        this.left = null;
+        this.right = null;
+        this.data = data;
+    }
+
+    public Node(Node parent, ProgramaNetflix data, Node left, Node right) {
+        this.parent = parent;
+        this.left = left;
+        this.right = right;
+        this.data = data;
+    }
+
+    public Node(ProgramaNetflix data, Node left, Node right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+    
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+        left.setParent(this);
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+        right.setParent(this);
+    }
+
+    public void setData(ProgramaNetflix data) {
+        this.data = data;
+    }
+    
+    public Node getParent() {
+        return this.parent;
+    }
+    
+    public Node getLeft() {
+        return this.left;
+    }
+    
+    public Node getRight() {
+        return this.right;
+    }
+
+     public ProgramaNetflix getData() { //TODO
+        return this.data;
+    }
+
+    public boolean hasLeft() {
+        return this.left != null;
+    }
+
+    public boolean hasRight() {
+        return this.right != null;
+    }
+
+    public boolean isRoot() {
+        return this.parent == null;
+    }
+
+    public boolean isLeaf() {
+        return this.left == null && this.right == null;
+    }
+
+	public int getDegree() {
+		int degree = 0;
+
+		if (hasLeft()) {
+            ++degree;
+        }
+
+		if (hasRight()) {
+            ++degree;
+        }
+
+		return degree;
+	}
+
+    public int getLevel() {
+		if (isRoot()) {
+			return 0;
+		}
+
+		return parent.getLevel() + 1;
+	}
+
+	public int getHeight() {
+		if (isLeaf()) {
+			return 0;
+		}
+
+        int height = 0;
+
+		if (hasLeft())
+			height = Math.max(height, left.getHeight());
+
+		if (hasRight())
+			height = Math.max(height, right.getHeight());
+
+		return height + 1;
+	}
+}
