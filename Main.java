4a3ae3d043e7dec524fa2014333817f0768fd8b0
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2023-11-17 20:53:34 -0300

    Change menu behavior to only display available options

--- Main.java
+++ Main.java
@@ -39,12 +39,16 @@ public class Main {
             System.out.println();
             System.out.println(new String(new char[12]).replace("\0", "#") + " MENU " + new String(new char[12]).replace("\0", "#"));
             System.out.println("1. Ler dados de arquivo");
-            System.out.println("2. Analisar Dados");
-            System.out.println("3. Inserir Programa");
-            System.out.println("4. Buscar Programa");
-            System.out.println("5. Remover Programa");
-            System.out.println("6. Exibir a Altura das Árvores");
-            System.out.println("7. Salvar Dados em Arquivo");
+
+            if (maxCompleted > 1) {
+                System.out.println("2. Analisar Dados");
+                System.out.println("3. Inserir Programa");
+                System.out.println("4. Buscar Programa");
+                System.out.println("5. Remover Programa");
+                System.out.println("6. Exibir a Altura das Árvores");
+                System.out.println("7. Salvar Dados em Arquivo");
+            }
+
             System.out.println("8. Encerrar a Aplicação");
             System.out.println();
             System.out.print("Insira a opção desejada: ");
