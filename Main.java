ce55545bcab483d3b65dac30f3367fbef1f3969c
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2023-11-20 13:30:12 -0300

    Add stats options to the menu

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Scanner;
 
 public class Main {
@@ -38,18 +40,25 @@ public class Main {
 
             System.out.println();
             System.out.println(new String(new char[12]).replace("\0", "#") + " MENU " + new String(new char[12]).replace("\0", "#"));
-            System.out.println("1. Ler dados de arquivo");
+            System.out.println(" 1. Ler dados de arquivo");
 
             if (maxCompleted > 1) {
-                System.out.println("2. Analisar Dados");
-                System.out.println("3. Inserir Programa");
-                System.out.println("4. Buscar Programa");
-                System.out.println("5. Remover Programa");
-                System.out.println("6. Exibir a Altura das Árvores");
-                System.out.println("7. Salvar Dados em Arquivo");
+                System.out.println();
+                System.out.println(" 2. Séries de animes com mais de uma temporada e classificação adequada para crianças");
+                System.out.println(" 3. Documentários históricos com classificação +16");
+                System.out.println(" 4. Filmes com IMDB score acima de um valor inserido pelo usuário, que não são de origem dos Estados Unidos, Canadá ou Reino Unido:");
+                System.out.println(" 5. ");
+                System.out.println(" 6. Filmes com duração maior que três horas e com score alto");
+                System.out.println();
+
+                System.out.println(" 7. Inserir Programa");
+                System.out.println(" 8. Buscar Programa");
+                System.out.println(" 9. Remover Programa");
+                System.out.println("10. Exibir a Altura das Árvores");
+                System.out.println("11. Salvar Dados em Arquivo");
             }
 
-            System.out.println("8. Encerrar a Aplicação");
+            System.out.println("12. Encerrar a Aplicação");
             System.out.println();
             System.out.print("Insira a opção desejada: ");
 
@@ -143,11 +152,9 @@ public class Main {
                             titlesAVL.insert(title);
 
                             counter++; // TODO: remove
-                            maxCompleted = 1;
                         }
                     }
 
-                    System.out.println("Árvores criadas com sucesso!");
                     System.out.println("\nÁrvores criadas com sucesso!");
                     sleep();
                     System.err.printf("\nDEBUG: %d entries\n\n", counter); // TODO: remove
@@ -159,17 +166,64 @@ public class Main {
                         break;
                     }
 
+                    AVL stats1 = titlesAVL.stats1();
+                    System.out.println(stats1.preorderTraversal());
+
                     maxCompleted = 2;
                     break;
                 case "3": //TODO
+                    if (maxCompleted < 1) {
+                        System.out.println("\nOpção indisponível. É necessário ler os dados do arquivo antes de analisá-los.");
+                        break;
+                    }
+
+                    AVL stats2 = titlesAVL.stats2();
+                    System.out.println(stats2.preorderTraversal());
+
+                    maxCompleted = 3;
+                    break;
+                case "4": //TODO
+                    if (maxCompleted < 1) {
+                        System.out.println("\nOpção indisponível. É necessário ler os dados do arquivo antes de analisá-los.");
+                        break;
+                    }
+
+                    AVL stats3 = titlesAVL.stats3();
+                    System.out.println(stats3.preorderTraversal());
+
+                    maxCompleted = 4;
+                    break;
+                case "5": //TODO
+                    if (maxCompleted < 1) {
+                        System.out.println("\nOpção indisponível. É necessário ler os dados do arquivo antes de analisá-los.");
+                        break;
+                    }
+
+//                    AVL stats4 = titlesAVL.stats4();
+//                    stats4.preorderTraversal();
+
+                    maxCompleted = 5;
+                    break;
+                case "6": //TODO
+                    if (maxCompleted < 1) {
+                        System.out.println("\nOpção indisponível. É necessário ler os dados do arquivo antes de analisá-los.");
+                        break;
+                    }
+
+                    AVL stats5 = titlesAVL.stats5();
+                    System.out.println(stats5.preorderTraversal());
+
+                    maxCompleted = 6;
+                    break;
+                case "7": //TODO
                     if (maxCompleted < 1) {
                         System.out.println("\nOpção indisponível. É necessário ler os dados do arquivo antes de inserir novos programas.");
                         break;
                     }
 
-                    maxCompleted = 3;
+                    maxCompleted = 7;
                     break;
-                case "4": //TODO
+                case "8": //TODO
                     if (maxCompleted < 1) {
                         System.out.println("\nOpção indisponível. É necessário ler os dados do arquivo antes de realizar buscas.");
                         break;
@@ -180,22 +234,34 @@ public class Main {
                     String searchId = m.nextLine();
                     System.out.println();
 
+
                     try {
+                        Instant bstStart = Instant.now();
+                        Node bstResult = titlesBST.search(searchId);
+                        Instant bstEnd = Instant.now();
+                        Duration bstTimeElapsed = Duration.between(bstStart, bstEnd);
+
+
                         Instant avlStart = Instant.now();
-                        Node result = titlesBST.search(searchId);
+                        Node avlResult = titlesAVL.search(searchId);
                         Instant avlEnd = Instant.now();
                         Duration timeElapsed = Duration.between(avlStart, avlEnd);
 
-                        System.out.printf("Tempo de busca via BST: %d ns%n%n", timeElapsed.toNanos());
+                        System.out.println(avlResult.getData().toString());
 
-                        System.out.println(result.getData().toString());
+                        System.out.printf("%nTempo de busca via BST: %d ns%n", bstTimeElapsed.toNanos());
+                        System.out.printf("Tempo de busca via AVL: %d ns%n", timeElapsed.toNanos());
+
+                        System.out.printf("%nNúmero de comparações via BST: %d", titlesBST.getNumOfComparisons());
+                        System.out.printf("%nNúmero de comparações via AVL: %d%n", titlesAVL.getNumOfComparisons());
                     } catch (NullPointerException e) {
                         System.out.printf("Programa %s não encontrado.%n", searchId);
                     }
 
-                    maxCompleted = 4;
+                    maxCompleted = 8;
+                    sleep(2500);
                     break;
-                case "5": //TODO
+                case "9": //TODO
                     if (maxCompleted < 1) {
                         System.out.println("\nOpção indisponível. É necessário ler os dados do arquivo antes de realizar remoções.");
                         break;
@@ -207,33 +273,36 @@ public class Main {
                     System.out.println();
 
                     try {
-                        titlesBST.remove(removeId);
+                        titlesAVL.remove(removeId);
                     } catch (NullPointerException e) {
                         System.out.printf("Programa %s não encontrado.%n", removeId);
                     }
 
-                    maxCompleted = 5;
+                    maxCompleted = 9;
+                    sleep();
                     break;
-                case "6": //TODO
+                case "10": //TODO
                     if (maxCompleted < 1) {
                         System.out.println("\nOpção indisponível. É necessário ler os dados do arquivo antes de calcular a altura das árvores.");
                         break;
                     }
 
                     System.out.printf("Altura da BST: %d%n", titlesBST.getHeight());
-//                    System.out.printf("Altura da AVL: %d%n", titlesAVL.getHeight());
+                    System.out.printf("Altura da AVL: %d%n", titlesAVL.getHeight());
 
-                    maxCompleted = 6;
+                    maxCompleted = 10;
+                    sleep();
                     break;
-                case "7": //TODO
+                case "11": //TODO
                     if (maxCompleted < 1) {
                         System.out.println("\nOpção indisponível. É necessário ler os dados do arquivo antes de realizar buscas.");
                         break;
                     }
 
-                    maxCompleted = 7;
+                    maxCompleted = 11;
+                    sleep();
                     break;
-                case "8":
+                case "12":
                     m.close();
                     System.exit(0);
                 default:
